91ed9844fc3da68af1b6a2ab21d4d599a64853c3
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2022-06-01 10:41:35 +0700

    Apply form group to set initial value

--- src/app/regiter/regiter.component.ts
+++ src/app/regiter/regiter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-regiter',
@@ -6,15 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./regiter.component.scss']
 })
 export class RegiterComponent implements OnInit {
-
-  constructor() { }
-
+  @ViewChild('confirmPassword', {static: false}) confirmPassword: ElementRef | undefined;
   genderValues = [
     {name: 'Laki-laki', value: 'male'},
     {name: 'Perempuan', value: 'female'}
-  ]
+  ];
+
+  userRegistrationForm: FormGroup;
+
+  constructor() {
+    this.userRegistrationForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.userRegistrationForm.valid && this.userRegistrationForm.value.password === this.confirmPassword?.nativeElement.value) {
+      console.log(this.userRegistrationForm.value)
+    } else {
+      console.log('Password tidak sama')
+    }
   }
 
 }
