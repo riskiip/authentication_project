9bfff7a039c9cfa850cadd69719c9bee5776d80f
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2022-06-01 09:42:12 +0700

    Generate login, register and dashboard component. Also register them to app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegiterComponent} from "./regiter/regiter.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {WildCardComponent} from "./wild-card/wild-card.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegiterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', component: WildCardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegiterComponent } from './regiter/regiter.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WildCardComponent } from './wild-card/wild-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegiterComponent,
+    DashboardComponent,
+    WildCardComponent
   ],
   imports: [
     BrowserModule,
