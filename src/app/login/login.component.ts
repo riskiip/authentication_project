10b1e20063b3444a7ab10a7f5f01a39ab1a9079c
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2022-06-03 21:58:21 +0700

    Create login and dashboard component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+    console.log(this.loginForm.value)
+      this.router.navigate(['dashboard']);
+    } else {
+
+    }
   }
 
 }
